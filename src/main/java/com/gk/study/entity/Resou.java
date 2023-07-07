9347eb150429798b4eea1b2e3331d8c26170fd60
gytide <2142780749@qq.com>
2023-07-07 20:31:03 +0800

    拉取合并资源管理

---
+++ src/main/java/com/gk/study/entity/Resou.java
@@ -0,0 +1,19 @@
+package com.gk.study.entity;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@TableName("b_resource")
+public class Resou implements Serializable {
+    @TableId(value = "id",type = IdType.AUTO)
+    public Long id;
+    @TableField
+    public String link;
+    @TableField
+    public String createTime;
+}
